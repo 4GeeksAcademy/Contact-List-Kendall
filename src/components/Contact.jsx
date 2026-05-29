bb8553b9b98d6287195c1fedd2e7242391cacde4
Kenube21 <kendallsh51@gmail.com>
2026-05-29 05:32:46 +0000

    newDetails

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
     const { store, deleteContact } = useGlobalReducer();
@@ -10,19 +10,21 @@ const Contact = () => {
                 <li key={contact.id}>
                     <div className="card mb-3">
                         <div className="row g-0">
-                            <div className="col-md-4">
-                                <img src="..." className="img-fluid rounded-start" alt="..." />
+                            <div className="text-center col-md-3 p-4">
+                                <img src={`https://i.pravatar.cc/200?img=${contact.id}`} className="img-fluid rounded-circle" alt="avatar" />
                             </div>
-                            <div className="col-md-8">
+                            <div className="col-md-6 p-2">
                                 <div className="card-body">
                                     <h5 className="card-title">{contact.name}</h5>
-                                    <p className="card-text">{contact.email}</p>
-                                    <p className="card-text">{contact.phone}</p>
-                                    <p className="card-text">{contact.address}</p>
+                                    <p className="card-text text-muted">{contact.email}</p>
+                                    <p className="card-text text-muted">{contact.phone}</p>
+                                    <p className="card-text text-muted">{contact.address}</p>
                                 </div>
                             </div>
-                            <button className="btn btn-danger" onClick={() => deleteContact(contact.id)}>Delete</button>
-                            <Link to={"/editContact/" + contact.id} className="btn btn-primary">Edit</Link>
+                            <div className="d-inline-flex align-self-start text-center col-md-3 p-4 gap-2">
+                                <Link to={"/editContact/" + contact.id} className="btn btn-primary">Edit</Link>
+                                <button className="btn btn-danger" onClick={() => deleteContact(contact.id)}>Delete</button>
+                            </div>
                         </div>
                     </div>
                 </li>
